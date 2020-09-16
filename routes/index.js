a67007d1e58a7c19e93555c2ab9e904b6af78188
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2020-09-16 19:41:04 +0700

    [test][s]: adds tests for datastore_search.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
 var express = require('express')
 var router = express.Router()
 
+
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
+
+const APP_VERSION = 'v1.1'
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.send('Hello world!')
+})
+
+
+/* GET . */
+router.get(`/${APP_VERSION}/datastore_search`, function (req, res, next) {
+  /*TODO*/
+  /* Auth handling  ... maybe JWT? */
+  /* query DB */
+  /* */
+  /* form result JSON */
+
+  /* parse req */
+  const resource_id = req.query.resource_id
+  // TODO react on no resource_id
+
+  console.log(req)
+  console.log('query: ' + req.query)
+  pool
+    .query('SELECT * FROM $1 LIMIT 100', [resource_id])
+    .then(result => {
+      // Convert pg.Result to JSON
+
+      // TODO: prep schema
+
+
+      console.log('pg result: ', result)
+
+      res.send({
+        data: result
+      })
+    })  
+    .catch(err => console.error('Error executing query', err.stack))
+
+  // client.release()
+  // res.send(result)
+  res.send(200)
 })
 
 module.exports = router
---
+++ test/integration/datastore_search.js
@@ -0,0 +1,98 @@
+const request = require('supertest')
+const app = require('../../app')
+var assert = require('assert')
+
+const APP_VERSION = 'v1.1'
+const DEFAULT_ROW_LIMIT = 100
+const TEST_TABLE_NAME = 'test_table'
+const TEST_TABLE_FIELDS = [
+  {
+    name: 'time_column',
+    type: 'timestamp',
+  },
+  {
+    name: 'text_column',
+    type: 'String',
+  },
+  {
+    name: 'float_column',
+    type: 'float8',
+  },
+  {
+    name: 'int_column',
+    type: 'Int',
+  },
+]
+
+describe('datastore_search endpoint', function () {
+
+  it('returns 200 in a basic case', function(done) {
+    request(app)
+      .get(`/${APP_VERSION}/datastore_search`)
+      .expect(200, done)
+  })
+
+
+  it('respond with default number of rows when requesting by resource_id on a table with more than the default rows', function (done) {
+    request(app)
+      .get(`/${APP_VERSION}/datastore_search?resource_id=${TEST_TABLE_NAME}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+        const jsonResp = JSON.parse(res.text)
+        if (!(jsonResp.data.length === DEFAULT_ROW_LIMIT)) {
+          throw new Error('response length is not correct')
+        }
+        return done()
+      })
+  })
+
+  it('return 404 when no such resource_id exist', function (done) {
+    request(app)
+      .get(`/${APP_VERSION}/datastore_search?resource_id=nonexsting_id`)
+      .expect(404, done)
+  })
+
+  it('should return a valid JSON by default', function (done) {
+    request(app)
+      .get(`/${APP_VERSION}/datastore_search?resource_id=${TEST_TABLE_NAME}`)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+        JSON.parse(res.text)
+        return done()
+      })
+  })
+
+  it('should return all table fields by default', function (done) {
+    request(app)
+      .get(`/${APP_VERSION}/datastore_search?resource_id=${TEST_TABLE_NAME}`)
+      .end((err, res) => {
+        const jsonResp = JSON.parse(res.text)
+
+        const resultFieldNames = jsonResp.schema.fields.map((f) => f.name)
+
+        assert.deepStrictEqual(resultFieldNames, TEST_TABLE_FIELDS)
+        return done()
+      })
+  })
+
+  // it('should return only different records when asking for disctinct in the request', function(done) {
+
+  //   return done();
+  // })
+
+  // it('should not include the total matching record count by default', function(done) {
+
+  //   return done();
+  // })
+
+  // it('', function(done) {
+
+  // })
+})
