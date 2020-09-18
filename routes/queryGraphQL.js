7a0d0ee7dfcb0f7a857e4bfac76a825dfc4f442d
Leo Rocha <leo.m.rocha@gmail.com>
2020-09-18 13:32:29 +0200

    [query-parameters][m] Added more query parameters to the query generation: offset and distinct_on, test passing

--- routes/queryGraphQL.js
+++ routes/queryGraphQL.js
@@ -42,7 +42,63 @@ function buildQueryForData(schema, params) {
   }`
 }
 
+/**
+ *
+ * @param {*} schema table schema in frictionless format
+ * @param {*} params query parameters, e.g. resource_id, fields, limit
+ * @returns String GraphQL query with optional params for fields being equal
+ */
+function buildParametrableQuery(schema, params) {
+  // $text_column: String
+  let variablesDeclaration = schema.fields
+    .map((f) => `$${f.name}: ${f.type}`)
+    .join(' ')
+
+  // text_column: { _eq: $text_column }
+
+
+  const whereFieldsList = schema.fields.map((f) => `${f.name}: { _eq: $${f.name} }`)
+  // console.log('whereFieldsList: ' + whereFieldsList)
+
+  const limitField = `limit: ${params.limit}`
+  // console.log('limitField: ' + limitField)
+  const whereField = `where: {
+      ${whereFieldsList.join(' ')}
+    }`
+    // console.log('whereField' + whereField)
+
+  const offsetField = params.offset ? `offset: ${params.offset}` : undefined
+  // console.log('offsetField: ' + offsetField)
+  const distinctField = params.distinct_on? `distinct_on:[${params.distinct_on}]` : undefined 
+  // console.log('disticntField: ' + distinctField)
+
+  const fields = [limitField, whereField, offsetField, distinctField].filter(el => el === 0 || Boolean(el))
+  console.log('fields' + fields)
+  // const orderField = schema.order_by? schema.order_by.map((f) => `${f.name}: { _eq: $${f.name} }`): ''
+  /*
+  query MyQuery {
+    test_table(
+      distinct_on: float_column, 
+      limit: 10, 
+      offset: 10, 
+      order_by: {float_column: asc, int_column: asc, text_column: asc, time_column: asc}, 
+      where: {float_column: {_eq: "2.2"}, 
+      int_column: {_eq: 6}, 
+      text_column: {_eq: "asd"}})
+  }
+
+  */
+
+  return gql`query ${params.resource_id}_query (${variablesDeclaration}) { 
+    ${params.resource_id}( ${fields}
+    ) {
+      ${params.fields.join(', ')}
+    }
+  }`
+}
+
 module.exports = {
   queryForData,
   buildQueryForData,
+  buildParametrableQuery
 }
--- test/integration/testQueryGraphQL.js
+++ test/integration/testQueryGraphQL.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const { buildQueryForData, queryForData } = require('../../routes/queryGraphQL')
+const { buildQueryForData, queryForData, buildParametrableQuery } = require('../../routes/queryGraphQL')
 var assert = require('assert')
 const { gql } = require('graphql-request')
 const expect = require('chai').expect
@@ -7,6 +7,7 @@ const expect = require('chai').expect
 describe('graphqlQueryBuilder', function () {
   describe('function queryForData', function () {
     it('should return 404 for non existing page', function (done) {
+      // TODO something is missing here
       console.log(process.env.HASURA_URL)
       done()
 
@@ -50,4 +51,57 @@ describe('graphqlQueryBuilder', function () {
       expect(result).to.include('time_column: { _eq: $time_column }')
     })
   })
+
+
+  describe('function buildParametrableQuery', function () {
+    const mockFrictionlessTableSchema = {
+      fields: [
+        {
+          name: 'float_column',
+          type: 'float8',
+        },
+        {
+          name: 'int_column',
+          type: 'Int',
+        },
+        {
+          name: 'text_column',
+          type: 'String',
+        },
+        {
+          name: 'time_column',
+          type: 'timestamp',
+        },
+      ],
+    }
+  
+    it('builds a result query with all possible query params', function () {
+  
+      const result = buildParametrableQuery(mockFrictionlessTableSchema, {
+        resource_id: 'test_table',
+        fields: ['int_column', 'text_column', 'float_column', 'time_column'],
+        limit: 11,
+        order_by: {'int_column':'asc', 'text_column':'desc', 'float_column':'desc', 'time_column':'asc'},
+        distinct_on: 'int_column',
+        offset: 5
+      })
+      console.log("Parametrable query built: " + JSON.stringify(result))
+      expect(result).to.be.a('string')
+      expect(result).to.include('float_column: { _eq: $float_column }')
+      expect(result).to.include('int_column: { _eq: $int_column }')
+      expect(result).to.include('text_column: { _eq: $text_column }')
+      expect(result).to.include('time_column: { _eq: $time_column }')
+      // limit
+      expect(result).to.include('limit: 11')
+      expect(result).to.include('offset: 5')
+      expect(result).to.include('distinct_on:[int_column]')
+      // TODO more conditions
+    })
+  
+  })
+  
+
 })
+
+
+
