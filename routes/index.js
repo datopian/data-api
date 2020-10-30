f5b148bd40ccc78c9bc26c74b37d3628c0e92794
Leo Rocha <leo.m.rocha@gmail.com>
2020-10-30 11:45:00 +0100

    [download-csv][s]CSV download working now, JSON to be fixed, xlsx should already work too

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 const { request, gql } = require('graphql-request')
 
 const { queryForData } = require('./queryGraphQL')
-const { json2csv, json2xslx } = require('./download')
+const XLSX = require('xlsx')
 
 const APP_VERSION = 'v1'
 
@@ -119,61 +119,48 @@ router.get(`/${APP_VERSION}/download`, async function (req, res, next) {
 /**
  *
  */
+
 router.post(`/${APP_VERSION}/download`, async function (req, res, next) {
   console.log(' Download CALLED')
   // res.send('THIS IS A RESPONSE')
   // get the graphql query from body
   // we might need to support maybe different formats: pure graphql or a json with fields detailing
   // AND a query field
-  console.log('body: ', req.body)
-  console.log('query: ', req.query)
-  console.log('route: ', req.route)
+  // console.log('body: ', req.body)
+  // console.log('query: ', req.query)
+  // console.log('route: ', req.route)
   // console.log(req)
   const query = req.body.query ? req.body.query : req.body
-  console.log('Body: ', query)
-  let result = {}
+  // console.log('QUERY: ', query)
   // call GraphQL
   try {
     // TODO check graphql syntax
     const gqlRes = await request(`${process.env.HASURA_URL}/v1/graphql`, query)
-    console.log('GraphQL response: ', gqlRes)
+    // console.log('Response type: ', typeof gqlRes, typeof JSON.stringify(gqlRes))
+    // console.log('GraphQL response: ', gqlRes)
+
     // // capture graphql response
     // // get query format type, default JSON
-    result = gqlRes // default response -> JSON, the same as graphql
-    if (req.params.format) {
-      const fmt = req.params.format.toLocaleLowerCase()
-      if (fmt == 'csv') {
-        result = json2csv(gqlRes)
-      } else if (fmt == 'xlsx') {
-        result = json2xslx(gqlRes)
-      }
-    } // else is by default the JSON
+    // result = gqlRes // default response -> JSON, the same as graphql
+    const ext = (req.params.format || 'csv').toLowerCase()
+    res.setHeader(
+      'Content-Disposition',
+      'attachment; filename="download.' + ext + '";'
+    )
+    let wb = XLSX.utils.book_new()
+    //iterate over the result sets and create a work sheet to append to the book
+    Object.keys(gqlRes).map((k) => {
+      const ws = XLSX.utils.json_to_sheet(gqlRes[k])
+      XLSX.utils.book_append_sheet(wb, ws, k)
+    })
+
+    // console.log('workbook created from json: ', wb)
+    res.end(XLSX.write(wb, { type: 'buffer', bookType: ext }))
+    // res.send(result)
   } catch (e) {
     console.error('Error during graphql call', e)
+    res.status(500).end()
   }
-  // stream result back to client
-  // const result = 'This is the result' // TODO erase this line
-  console.log('RESULT: ', result)
-
-  // // official express doc: https://expressjs.com/en/api.html#res.attachment
-  res.attachment()
-  res.type('json')
-  // res.attachment() -> this might be the one I'm looking for with some tweaks, should set res.type() first to a file
-  // then create the file bynary as a stream and stream chunks through res.write() till there is nothing else.
-  // need to handle backpressure (check link below) while the general download is something like: https://www.semicolonworld.com/question/44449/download-a-file-from-nodejs-server-using-express
-  // once the file is
-  // res.download() -> this is for a file on disk  uses res.sendFile()
-  // res.sendFile() -> this is for a file on disk
-  // can check here: https://github.com/expressjs/express/blob/master/lib/response.js#L493 how sendFile handles the setup
-  // Excellent documentation here:
-  // Check for Streams: https://nodesource.com/blog/understanding-streams-in-nodejs/
-  // We could also handle compression
-  // zlib.createDeflate() compress data using deflate (a compression algorithm) into a stream
-  // res IS a stream and as such can handle streaming responses.
-  // What could be done is chunk the file and write it handling the backpressure
-  res.send(JSON.stringify(result))
-  // res.write(result)
-  // res.end()
 })
 
 module.exports = router
